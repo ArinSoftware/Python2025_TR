f64d9aee9eaedf468599e01ef070abf5bfe651a3
gurcancekic <arinyazilim@gmail.com>
2024-12-08 19:02:25 +0300

    Lesson 09

---
+++ lesson09.py
@@ -0,0 +1,43 @@
+
+""" 1
+Bir öğrencinin bilgileri için aşağidaki değişkenleri oluşturun ve yazdirin:
+
+- Adinizi içeren bir student_name değişkeni oluşturun.
+- Yaşinizi içeren bir student_age değişkeni oluşturun.
+- True değerine sahip bir is_student değişkeni oluşturun.
+- Üç değişkenin hepsini ayni satir içerisinde yazdiriniz.
+
+"""
+
+""" 2
+x, y, z değişkenlerine ayni değeri atayin ve yazdirin.
+
+"""
+
+""" 3
+course adinda bir değişken oluşturun ve değerini "Python Programming" olarak belirleyin.
+course değerini büyük harflerle yazdirin.
+course değerini küçük harflerle yazdirin.
+course değerinin uzunluğunu yazdirin.
+course değerinin ilk 6 karakterini yazdirin.
+"""
+
+""" 4
+
+language = "Python" adinda bir değişken oluşturun.
+Değişkenin ilk karakterini yazdirin.
+Değişkenin son karakterini yazdirin.
+Değişkenin 1. indexten 4. indexe kadar olan karakterlerini yazdirin.
+Değişkenin her iki karakterden birini yazdirin.
+"""
+
+""" 5
+Bir oyunucunun başlangiçta 100 puani vardir.
+Oyuncu her canavarla karşilaştiğinda 30 puan kaybeder ve her canavari 
+yendiğinde +10 puan alir. Oyuncunun sağliği 0 veya altina düştüğünde oyun 
+sona erer ve öldürülen toplam canavar sayisi kaçtir.
+
+"""
+
+
+
