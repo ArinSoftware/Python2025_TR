15b3c97cd95ba5a3bc8c2fd2962f98cb76681128
gurcancekic <arinyazilim@gmail.com>
2024-12-22 14:43:00 +0300

    Lesson 12

---
+++ lesson12.py
@@ -0,0 +1,50 @@
+#Matematiksel İşlemler (Math Operations)
+
+#Toplama - Addition
+a = 10
+b = 5
+result = a + b
+print(f"The result is: {result}")
+
+#Çıkarma - Substruction
+result = a-b
+print(f"The result is: {result}")
+
+#Çarpma - Multiplication
+result = a*b
+print(f"The result is: {result}")
+
+#Bölme - Division
+result = a/b
+print(f"The result is: {result}") # Result type is float by default
+
+#Tam Sayılı Bölme - Floor Division
+result = a//b
+print(f"The result is: {result}")
+
+#Kalan - Modulus
+result = a%b
+print(f"The result is: {result}")
+
+#İşlem Sırası - Order of operations(PEMDAS)
+#Parantezler
+#Üslü İfadeler
+#Çarpma Bölme (Soldan Sağa)
+#Toplama Çıkarma (Soldan Sağa)
+
+result1 = (3 + 5)**2 / (2 *5) + 10 - 4 # 12.4
+result2 = (2 * (2 + 5) - 4) / (2 ** 2) # 3.0
+
+#Compound assignment operators
+a = 10
+a += 5 # 15
+b = 10
+b -= 5 # 5
+
+c = 10
+c *= 5 # 50
+
+d = 10
+d /= 5 # 2.0
+
+
